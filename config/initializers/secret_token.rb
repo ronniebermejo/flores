b7829e34876da98b70c4e45f1bbfa072069b0404
ronnie_bermejo <ronnie.bermejo.mx@gmail.com>
2013-06-09 09:06:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FloresDeBach::Application.config.secret_token = 'f85c5806aa9cdc4d9189665af43c69bbb343ad548a61229f9a3834031759593345d6d20d3f53deeea72aca15158303482590aff9c6d50ad410cd843f3d134a33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FloresDeBach::Application.config.session_store :cookie_store, key: '_flores_de_bach_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FloresDeBach::Application.config.session_store :active_record_store
